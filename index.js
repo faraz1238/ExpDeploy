fce5e99b187fc79c7e6851e6263c582e8cedf475
FARAZ TANWIR <126073683+faraz1238@users.noreply.github.com>
2024-05-22 14:53:15 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+
+const app = express()
+
+
+app.get('/', (req, res) => {
+  res.send('Express vercel app response')
+})
+
+app.listen(5000, () => {
+  console.log(`app listening on port 5000`)
+})
